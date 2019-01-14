2b3ba8709ee9f72ff15a7b930821b384a625db37
ignesious <mohammedtce@gmail.com>
2019-01-14 18:58:11 +0530

    Final Commit

--- TrainingAce/src/com/training/Random/GenericShit.java
+++ TrainingAce/src/com/training/Random/GenericShit.java
@@ -7,7 +7,43 @@ import java.lang.*;
 import java.io.*;
 
 class A{
-  public void genericPrint(List<Number> intlist)
+	
+	int i;
+	int j;
+	String k;
+	
+  @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + i;
+		result = prime * result + j;
+		result = prime * result + ((k == null) ? 0 : k.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		A other = (A) obj;
+		if (i != other.i)
+			return false;
+		if (j != other.j)
+			return false;
+		if (k == null) {
+			if (other.k != null)
+				return false;
+		} else if (!k.equals(other.k))
+			return false;
+		return true;
+	}
+
+public void genericPrint(List<Number> intlist)
   {
       
   }
