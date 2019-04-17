390298d32510e15f61e11b847bd76e4231ba1c1a
m0m049v <m0m049v@m-c02xf25wjg5h.idc.in.wal-mart.com>
2019-04-17 13:59:37 +0530

    kafka

---
+++ TrainingAce/src/Kafka/KafkaProducer.java
@@ -0,0 +1,17 @@
+package Kafka;
+
+import java.util.Properties;
+
+
+/*
+ * https://www.tutorialspoint.com/apache_kafka/apache_kafka_simple_producer_example.htm
+ */
+
+public class KafkaProducer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
